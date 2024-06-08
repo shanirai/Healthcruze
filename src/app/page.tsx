a73f278d57880ae7b5ba6a93a436d424dec728c0
shanirai <shani.rai@mckinleyrice.com>
2024-06-08 13:38:49 +0530

    initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+export default function HomePage({}: Props) {
+  return (
+    <div className="justify-center flex min-h-screen items-center ">
+      Welcome on HealthCruze
+    </div>
+  );
+}
